3ad785481dd4fd4080ceb85047a85a2dc542ae57
Rico Rueedi <metabomatching@unil.ch>
2018-01-20 12:42:02 +0100

    Docker fix 4.

--- metabomatching-test.sh
+++ metabomatching-test.sh
@@ -14,11 +14,11 @@ fi
 # =========
 
 # Test with a directory
-./metabomatching.sh -g "$PROG_DIR_NAME/test" -c  || exit 1
+metabomatching.sh -g "$PROG_DIR_NAME/test" -c  || exit 1
 
 # Test with an input file
 scores_file=$PROG_DIR_NAME/test/sco.tsv
 pdf_file=$PROG_DIR_NAME/test/sco.pdf
-./metabomatching.sh -g -i "$PROG_DIR_NAME/test/ps.test.multi/test.pseudospectrum.tsv" -c "" -p "$PROG_DIR_NAME/test/parameters.gxy.1.tsv" -s "$scores_file" -S "$pdf_file" || exit 2
+metabomatching.sh -g -i "$PROG_DIR_NAME/test/ps.test.multi/test.pseudospectrum.tsv" -c "" -p "$PROG_DIR_NAME/test/parameters.gxy.1.tsv" -s "$scores_file" -S "$pdf_file" || exit 2
 [ -f "$scores_file" ] || exit 3
 [ -f "$pdf_file" ] || exit 4
